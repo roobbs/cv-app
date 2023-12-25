1c0b95397d6469004c1825f4445d0122d9d5e317
Carlos <carlossalmeroncabrera@gmail.com>
2023-12-25 13:01:32 -0600

    Added Personal Info component and started Education component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 import PersonalData from "./personalData";
-function App() {
+import Education from "./education";
+
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <PersonalData />
-      <PersonalData />
+      <Education />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
@@ -18,5 +20,3 @@ function App() {
     </>
   );
 }
-
-export default App;
--- src/personalData.jsx
+++ src/personalData.jsx
@@ -1,9 +1,45 @@
-export default function Comp() {
+import { useState } from "react";
+import "./container.css";
+
+export default function PersonalData() {
+  const [showForm, setShowForm] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleNameChange = (e) => setFullName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleCityChange = (e) => setCity(e.target.value);
+  const handlePhoneChange = (e) => setPhone(e.target.value);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
-    <>
-      <div>Title</div>
-      <div>Hello World</div>
-      <br />
-    </>
+    <div className="infoContainer">
+      <h2 onClick={toggleForm}>Personal Information {showForm ? "▼" : "▶"}</h2>
+      {showForm && (
+        <form>
+          <label>
+            Full Name:
+            <input type="text" value={fullName} onChange={handleNameChange} />
+          </label>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={handleEmailChange} />
+          </label>
+          <label>
+            City:
+            <input type="text" value={city} onChange={handleCityChange} />
+          </label>
+          <label>
+            Phone Number:
+            <input type="tel" value={phone} onChange={handlePhoneChange} />
+          </label>
+        </form>
+      )}
+    </div>
   );
 }
